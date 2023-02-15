c0dd2915450d061392493358325f07972f4986fc
steve-moretz <sm.steve.moretz@gmail.com>
2023-02-15 15:10:14 +0330

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {gql, InMemoryCache, ApolloClient} from "@apollo/client";
+import {Button, View} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={{flex: 1, justifyContent: "center"}}>
+            <Button
+                title={"Press"}
+                onPress={async () => {
+                    try {
+                        const client = new ApolloClient({
+                            cache: new InMemoryCache({}),
+                            uri: "http://dummy.test",
+                        });
+
+                        await client
+                            .subscribe({
+                                query: gql(/* GraphQL */ `
+                                    subscription {
+                                        postUpdated {
+                                            ID
+                                            rooms
+                                        }
+                                    }
+                                `),
+                                errorPolicy: "all",
+                                context: {
+                                    uri: "http://graphql.test/central/api/graphql",
+                                },
+                            })
+                            .subscribe((value) => {
+                                console.log("subscribe", value);
+                            });
+                    } catch (e) {
+                        console.log("error", e);
+                    }
+                }}
+            />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
